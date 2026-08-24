258bf81829e26dd4d7607999c8c86070d352fcb1
v0lt <mpc-be@mail.ru>
2026-08-24 14:46:13 +0300

    Удален странный "#define printf sizeof".

--- src/Meia/source/MPEGFile.cpp
+++ src/Meia/source/MPEGFile.cpp
@@ -2,7 +2,7 @@
 // Video decoding library
 //
 // Copyright (C) 2013 Avery Lee
-// Copyright (C) 2025 v0lt
+// Copyright (C) 2025-2026 v0lt
 //
 // SPDX-License-Identifier: GPL-2.0-or-later
 //
@@ -21,8 +21,6 @@
 
 #include <vd2/Meia/MPEGFile.h>
 #include "MPEGCache.h"
-
-#define printf sizeof
 
 ///////////////////////////////////////////////////////////////////////////
 
@@ -755,7 +753,7 @@ public:
 		mpOnGOPHeader = &VDMPEGVideoStream::_ParseInitialGOPHeader;
 		mParseScanDesiredFrame = -1;
 
-		printf("restarting!!!\n");
+//		printf("restarting!!!\n");
 	}
 
 	void ParseBeginSeekScan(bool master) {
